90ff362f901c9ad94342682d70b366ddf9e66a29
king <3494569036@qq.com>
2022-10-29 20:00:01 +0800

    customerDao类方法的修改

--- wendao/src/main/java/com/kaifamiao/wendao/dao/CustomerDao.java
+++ wendao/src/main/java/com/kaifamiao/wendao/dao/CustomerDao.java
@@ -10,11 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CustomerDao extends BaseDao<Customer,Long>{
-    private String INSERT_ONE="INSERT INTO t_customers(username,password,salt,nickname,reg_date,id) VALUES(?,?,?,?,?,?)";
-    private String SELECT_MAX_ID="SELECT MAX(id) FROM t_customer";
-    private String Modify="UPDATE t_customers SET username=?,password=?,salt=?,nickname=? WHERE id=?";
+    private String INSERT_ONE="INSERT INTO t_customers(username,password,salt,nickname,reg_date,introduce,management,gender,id) VALUES(?,?,?,?,?,?,?,?)";
+    private String Modify="UPDATE t_customers SET username=?,password=?,salt=?,nickname=?,introduce=?,gender=?, WHERE id=?";
     private String DELETE="DELETE FROM t_customers WHERE id=?";
-    private String SELECT_ONE_BASE="SELECT username,password,salt,nickname,reg_date,id FROM t_customers WHERE";
+    private String SELECT_ONE_BASE="SELECT username,password,salt,nickname,reg_date,introduce,management,gender,id FROM t_customers WHERE";
     private String SELECT_ONE= SELECT_ONE_BASE+" id=?";
     private String SELECT_ONE_USERNAME=SELECT_ONE_BASE+" username=?";
     private String SELECT_ALL="SELECT username,password,salt,nickname,reg_date,id FROM t_customers ORDER BY id";
@@ -24,7 +23,7 @@ public class CustomerDao extends BaseDao<Customer,Long>{
         java.sql.Date date=java.sql.Date.valueOf(local);
         Object [] objects={
                 customer.getUsername(),customer.getPassword(),customer.getSalt(),customer.getNickname(),
-                date,customer.getId()
+                date,customer.getManagement(),customer.getGender(),customer.getId()
         };
         try {
             return runner.update(INSERT_ONE,objects)==1;
@@ -35,7 +34,7 @@ public class CustomerDao extends BaseDao<Customer,Long>{
     @Override
     public boolean modify(Customer customer) {
         Object [] objects={customer.getUsername(),customer.getPassword(),
-                customer.getSalt(),customer.getNickname(),customer.getId()};
+                customer.getSalt(),customer.getNickname(),customer.getIntroduction(),customer.getGender(),customer.getId()};
         try {
             return runner.update(Modify,objects)==1;
         } catch (SQLException cause) {
@@ -80,6 +79,9 @@ public class CustomerDao extends BaseDao<Customer,Long>{
                 cus.setSalt(rs.getString("salt"));
                 java.sql.Date date= rs.getDate("reg_date");
                 cus.setRegisterDate(date==null? null:date.toLocalDate());
+                cus.setGender(rs.getString("gender"));
+                cus.setManagement(rs.getInt("management"));
+                cus.setIntroduction(rs.getString("introduce"));
                 return cus;
             }
             return null;
