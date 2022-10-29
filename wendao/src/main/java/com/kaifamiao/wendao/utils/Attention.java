7dc384c412dacf29aed83923bba3413914a748db
王燮然 <wxr_bebetter@163.com>
2022-10-29 18:14:47 +0800

    粉丝关注工具类

---
+++ wendao/src/main/java/com/kaifamiao/wendao/utils/Attention.java
@@ -0,0 +1,33 @@
+package com.kaifamiao.wendao.utils;
+
+public class Attention {
+
+    private Long id;
+    private Long fans_id;
+    private Long attention_id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public Long getFans_id() {
+        return fans_id;
+    }
+
+    public void setFans_id(Long fans_id) {
+        this.fans_id = fans_id;
+    }
+
+    public Long getAttention_id() {
+        return attention_id;
+    }
+
+    public void setAttention_id(Long attention_id) {
+        this.attention_id = attention_id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
