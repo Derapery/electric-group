f757832b609b4226d20e009a5efcf3c55384d606
king <3494569036@qq.com>
2022-10-30 17:31:06 +0800

    修改结果集的处理和方法

--- wendao/src/main/java/com/kaifamiao/wendao/dao/AttentionDao.java
+++ wendao/src/main/java/com/kaifamiao/wendao/dao/AttentionDao.java
@@ -14,9 +14,22 @@ public class AttentionDao extends BaseDao<Attention,Long>{
 
     private static final String INSERT_ONE="INSERT INTO  t_fans(id,follwer_id,up_id) VALUES(?,?,?)";
     private static final String DELETE = "DELETE FROM t_fans WHERE follwer_id = ? and up_id = ?";
-    private static final String FIND_BY_ATTENTION = "SELECT up_id , follwer_id FROM t_category WHERE follwer_id = ?";
-    private static final String FIND_BY_FANS = "SELECT follwer_id , follwer_id FROM t_category WHERE up_id = ?";
+    private static final String FIND_BY_ATTENTION = "SELECT up_id FROM t_category WHERE follwer_id = ?";
+    private static final String FIND_BY_FANS = "SELECT  follwer_id FROM t_category WHERE up_id = ?";
     private static final String DELETE_CUSTOMER="DELETE FROM t_fans WHERE follwer_id=? AND up_id=?";
+    private CustomerDao customerDao=new CustomerDao();
+    private   ResultSetHandler<List<Customer>> rsHandler = rs -> {
+        List<Customer> list = new ArrayList<>();
+        if( rs.next() ) {
+            Customer c = wrap(rs);
+            list.add(c);
+        }
+        return list;
+    };
+    private Customer wrap(ResultSet resultSet) throws SQLException {
+            Customer customer=customerDao.find(resultSet.getLong(1));
+            return customer;
+    }
     //添加关注的信息到表中
     @Override
     public boolean save(Attention attention) {
@@ -61,43 +74,20 @@ public class AttentionDao extends BaseDao<Attention,Long>{
     public List<Attention> finaAll() {
         return null;
     }
-
+    //关注用户列表
     public List<Customer> findByCustomer(Customer customer) {
-        ResultSetHandler<List<Customer>> rsHandler = rs -> {
-            List<Customer> list = new ArrayList<>();
-            if( rs.next() ) {
-                Customer c = wrap(rs);
-                list.add(c);
-            }
-            return list;
-        };
         try {
             return runner.query(FIND_BY_ATTENTION,rsHandler,customer.getId());
         } catch (SQLException cause) {
             throw new RuntimeException("查询关注列表失败", cause);
         }
     }
-
+    //自己的粉丝的列表
     public List<Customer> findByFans(Customer customer) {
-        ResultSetHandler<List<Customer>> rsHandler = rs -> {
-            List<Customer> list = new ArrayList<>();
-            if( rs.next() ) {
-                Customer c = wrap(rs);
-                list.add(c);
-            }
-            return list;
-        };
         try {
             return runner.query(FIND_BY_FANS,rsHandler,customer.getId());
         } catch (SQLException cause) {
             throw new RuntimeException("查询粉丝列表失败", cause);
         }
     }
-
-    private Customer wrap(ResultSet rs) throws SQLException {
-        Customer c = new Customer();
-        c.setId(rs.getLong("up_id"));
-        return c;
-    }
-
 }
