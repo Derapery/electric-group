fba44eee99de2df39a7097978a47ea8fa2d5bbe9
王燮然 <wxr_bebetter@163.com>
2022-10-31 16:02:11 +0800

    文件上传配置

--- wendao/src/main/webapp/js/editor.js
+++ wendao/src/main/webapp/js/editor.js
@@ -2,7 +2,12 @@
 let options = {
     // 通过CSS选择器命中表单中的<textarea>元素
     textarea: $('#editor'),
-    placeholder: '请输入内容...'
+    placeholder: '请输入内容...',
+    upload: {
+        url: '/file/simditor/upload',
+        fileKey: 'image',
+        leaveConfirm: '图片正在上传中，你确定要离开当前页面吗？'
+    }
 }
 
 let editor = new Simditor(options);
