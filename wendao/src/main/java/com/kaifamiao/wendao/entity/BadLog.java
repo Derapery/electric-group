1581d06b24d6b4449adbad1607aa4f12974fed62
王燮然 <wxr_bebetter@163.com>
2022-11-04 00:43:11 +0800

    不良信息实体类

---
+++ wendao/src/main/java/com/kaifamiao/wendao/entity/BadLog.java
@@ -0,0 +1,31 @@
+package com.kaifamiao.wendao.entity;
+
+public class BadLog {
+    private Long id;
+    private Long user_id;
+    private Integer type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getUser_id() {
+        return user_id;
+    }
+
+    public void setUser_id(Long user_id) {
+        this.user_id = user_id;
+    }
+
+    public Integer getType() {
+        return type;
+    }
+
+    public void setType(Integer type) {
+        this.type = type;
+    }
+}
